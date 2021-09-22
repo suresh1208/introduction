36f5ed39e145b1f225ffa4a076191e2f708eeb21
suresh1208 <sidhusuresh1@gmail.com>
2021-09-22 19:25:46 +0530

    add the lines

---
+++ src/org/test/Drop.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Drop {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sidhu\\eclipse-workspace\\Selenium9\\drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://en-gb.facebook.com/");
+		
+	
+	
+	
+	
+	}
+
+}
